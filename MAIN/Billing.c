c8ee85cca2ccb4d50297ce0760899e288dc6638b
SAVANI VEDANT VIJAY <Vedant.Vijay@iiitb.ac.in>
2024-11-17 17:31:26 +0530

    BILLING

--- MAIN/Billing.c
+++ MAIN/Billing.c
@@ -1,6 +1,20 @@
 #include "../Code_Snippets/String_Alignment/f_string.h"
-
+#include <stdio.h>
 int main()
 {
-    f_string_format(1,"THIS IS IN BETWEEEN");
+    // clear_console();
+    print_border("-");
+    
+    f_string_format(0,"\033[1;38;2;255;165;0mTrivago\033[0m\n");
+    char arr[100];
+    date_d(arr,sizeof(arr));
+    f_string_format(2,"%s",arr);
+    print_border("-");
+    printf("\n");
+    f_string_format(1,"\033[1;38;2;0;100;0mBilling Information\033[0m\n");
+
+    printf("")
+    printf("\t");
+
+
 }
