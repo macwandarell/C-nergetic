35da507c81ad9b1f90b3ba51d5867e61b2ce69f8
Vedansh Patel <185171791+1Vedansh@users.noreply.github.com>
2024-11-21 23:46:17 +0530

    Login almost done.

--- MAIN/Login.c
+++ MAIN/Login.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#define SLEEP(seconds) Sleep((seconds) * 1000)
+#else
+#include <unistd.h>
+#define SLEEP(seconds) sleep(seconds)
+#endif
+
+
 
 typedef struct Customer{
     char username[51];
@@ -9,15 +18,6 @@ typedef struct Customer{
     char password[31];
     struct Customer *next;
 }Customer;
-
-void printCustomers(Customer* head) {
-    Customer* current = head;
-    while (current != NULL) {
-        printf("Username: %s, Email: %s, Password: %s\n", current->username, current->email, current->password);
-        current = current->next;
-    }
-}
-
 
 Customer* createCustomer(const char* username, const char* email, const char* password) {
     Customer* newCustomer = (Customer*) malloc (sizeof(Customer));
@@ -44,12 +44,10 @@ void appendCustomer(Customer** head, const char* username, const char* email, co
     }
 }
 
-
-
 void login() {
-    FILE* fp = fopen("output.csv", "r");
+    FILE* fp = fopen("Registration/output.csv", "r");
     if (!fp) {
-    printf("Error: Unable to open the file.\n");
+    printf("File does not exist.\n");
     return;
     }
     char buffer[1024];
@@ -68,14 +66,39 @@ void login() {
     fclose(fp);
 
     print_border("*");
-    f_string_format(1, "Welcome To The Registration Page!");
+    f_string_format(1, "Welcome To The Login Page!");
     char s[100];
     date_d(s,sizeof(s));
     f_string_format(2, "%s        ",s);
     print_border("*"); 
+    
+    int flag = 1;
+    while (flag){
+        char username[51];
+        char password[31];
+        printf("\n");
+        printf("Enter your username: ");
+        fgets(username, sizeof(username), stdin);
+        username[strcspn(username, "\n")] = '\0';
+        printf("Enter your password: ");
+        scanf("%s", password);
+        getchar();
 
-    printCustomers(customerList);
-    printf("HEllo");
+        Customer* current = customerList;
+        while (current != NULL) {
+            if (strcmp(current->username, username) == 0 && strcmp(current->password, password) == 0) {
+                f_string_format(1, "Login Successful!");
+                SLEEP(4);
+                clear_console();
+                flag = 0;
+                break;
+            }
+            current = current->next;
+        }
+        if (flag) {
+            f_string_format(1, "Invalid username or password. Please try again.");
+        }
+    }
 }
 
 int main() {
