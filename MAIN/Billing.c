6df30217fe290f75263eb33734055bb55fd4358c
SAVANI VEDANT VIJAY <Vedant.Vijay@iiitb.ac.in>
2024-11-23 00:09:49 +0530

    bill part 2

--- MAIN/Billing.c
+++ MAIN/Billing.c
@@ -40,13 +40,16 @@ int main()
     print_border("-");
     print_border("-");
     f_string_format(0,"\t\t\t\t\t\t Price for the Room :\t\t8756");
-    
-   
+    printf("\n"); 
     f_string_format(0,"\t\t\t\t\t\t Price for the stay :\t\t4343");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t Price for ext commodities :\tbed,pool");
+    printf("\n");
     f_string_format(0, "\t\t\t\t\t\t Discount for you :\t\t10");
+    printf("\n");
     int a=21;
     f_string_format(0,"\t\t\t\t\t\t Payment to be made :\t\t%d",a);
+    printf("\n");
     
 
    
@@ -54,12 +57,18 @@ int main()
     print_border("-"); 
     
     f_string_format(0,"\t\t\t\t\t\t   Check out :\t\t12/23/23");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t   Check In :\t\t12/13/23");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t   Hotel Name :\t\tTaj");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t   Location :\t\tMumbai");
+    printf("\n");
     int b=265651;
     f_string_format(0,"\t\t\t\t\t\t   Booking Date :\t%d",b);
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t   Commodities :\tnoo");
+    printf("\n");
     print_border("-");
     print_border("-");
     printf("\n ");
@@ -68,15 +77,24 @@ int main()
     scanf("%c",&confirm_payment);
     printf("\n");
     if(confirm_payment=='Y' || confirm_payment=='y')
-        f_string_format(0,"\t\t\t\t\t\t\t Payment Successful");
+    {    f_string_format(0,"\t\t\t\t\t\t\t Payment Successful");
+        printf("\n");
+    }
     else if(confirm_payment=='N' || confirm_payment=='n')
-        f_string_format(0,"\t\t\t\t\t\t\t Payment Failed");
+    {   f_string_format(0,"\t\t\t\t\t\t\t Payment Failed");
+        printf("\n");
+    }
     else
+    {
         f_string_format(0,"\t\t\t\t\t\t\t Invalid Input");
+        printf("\n");
+    }
     
-    printf("\n \n \n");
+    printf("\n \n");
     f_string_format(0,"\t\t\t\t\t\t\t Customer Care :");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t\t email id :");
+    printf("\n");
     f_string_format(0,"\t\t\t\t\t\t\t CompanyName | (C) ");
     
      SLEEP(2);
@@ -101,6 +119,7 @@ int main()
         print_border("-");
         print_border("-");
         printf("\t\t\t\t\t\t Choose the Payment Method :");
+        printf("\n");
         printf("\t\t\t\t\t\t 1. Credit Card");
         printf("\n");
         printf("\t\t\t\t\t\t 2. Debit Card");
@@ -124,32 +143,53 @@ int main()
         {
         case 1:
             printf("\t\t\t\t\t\t Enter the Credit Card Number :");
-            
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the CVV :");
             scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the Expiry Date :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Confirm the Payment?(Y/N):");
             scanf("%c",&confirm_last_time_payment);
+            printf("\n");
             if(confirm_last_time_payment=='N' || confirm_last_time_payment=='n')
-                printf("\t\t\t\t\t\t\t Payment Failed");
+               printf("\t\t\t\t\t\t\t Payment Failed");
+
             else
                 printf("\t\t\t\t\t\t\t Invalid Input");
             break;
         case 2:
             printf("\t\t\t\t\t\t Enter the Debit Card Number :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the CVV :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the Expiry Date :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Confirm the Payment?(Y/N):");
             scanf("%c",&confirm_last_time_payment);
+            printf("\n");
             if(confirm_last_time_payment=='N' || confirm_last_time_payment=='n')
                 printf("\t\t\t\t\t\t\t Payment Failed");
+                
             else
                 printf("\t\t\t\t\t\t\t Invalid Input");
+                
             break;
         case 3: 
             printf("\t\t\t\t\t\t Enter the Bank Name :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the Account Number :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Enter the IFSC Code :");
+            scanf("%s",details);
+            printf("\n");
             printf("\t\t\t\t\t\t Confirm the Payment?(Y/N):");
             scanf("%c",&confirm_last_time_payment);
             if(confirm_last_time_payment=='N' || confirm_last_time_payment=='n')
@@ -201,17 +241,26 @@ int main()
         }
         
 
+        print_border("\n");
+        print_border("-");
+        print_border("-");
+        printf("\n \n \n");
+        f_string_format(0,"\t\t\t\t\t\t\t Customer Care :");
+        printf("\n");
+        f_string_format(0,"\t\t\t\t\t\t\t email id :");
+        printf("\n");
+        f_string_format(0,"\t\t\t\t\t\t\t CompanyName | (C) ");
+        printf("\n");
 
-        print_border("-");
-        print_border("-");
+
 
 
     }
 
-    printf("\n \n \n");
-    f_string_format(0,"\t\t\t\t\t\t\t Customer Care :");
-    f_string_format(0,"\t\t\t\t\t\t\t email id :");
-    f_string_format(0,"\t\t\t\t\t\t\t CompanyName | (C) ");
+    // printf("\n \n \n");
+    // f_string_format(0,"\t\t\t\t\t\t\t Customer Care :");
+    // f_string_format(0,"\t\t\t\t\t\t\t email id :");
+    // f_string_format(0,"\t\t\t\t\t\t\t CompanyName | (C) ");
     
     return 0;
 }
